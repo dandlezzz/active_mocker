70b3709ef9deebb874973562e97887371ccd2917
Dustin Zeisler <dustin@zive.me>
2014-05-30 10:31:44 -0700

    scaffolding for has_one/belong_to creations

---
+++ lib/active_mocker/collection/has_one_belong_to.rb
@@ -0,0 +1,39 @@
+module ActiveMocker
+
+  module Collection
+
+    class HasOneBelongTo < Base
+
+      include Queries
+
+      # belongs_to
+
+      # association(force_reload = false)
+      # association=(associate)
+      # build_association(attributes = {})
+      # create_association(attributes = {})
+      # create_association!(attributes = {})
+      #
+      # customer
+      # customer=
+      # build_customer
+      # create_customer
+      # create_customer!
+
+      #has_one
+
+      # association(force_reload = false)
+      # association=(associate)
+      # build_association(attributes = {})
+      # create_association(attributes = {})
+      # create_association!(attributes = {})
+
+      # account
+      # account=
+      # build_account
+      # create_account
+      # create_account!
+
+    end
+  end
+end
